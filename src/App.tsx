97e0fa526ca14b13cd955f5b5bc171979f179c74
Your Name <you@example.com>
2021-10-27 16:49:30 +0300

    testing library login form initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, LoginForm } from "react-customer-support";
 
 function App() {
+  const okay: string = "hey";
+
+  const [user, setUser] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const loginHandler = () => {
+    console.log("hey");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button name="ellie" />
+      <LoginForm
+        joinRoom={async (okay: string) => {
+          console.log("hey");
+        }}
+        user={user}
+        setUser={setUser}
+        password={password}
+        setPassword={setPassword}
+        loginHandler={loginHandler}
+      />
     </div>
   );
 }
