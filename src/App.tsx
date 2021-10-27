f3eedeb7dad875dba48b8d06d0c729801b0b4d96
Your Name <you@example.com>
2021-10-27 17:44:26 +0300

    minimal changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, LoginForm } from "react-customer-support";
+import { Button, LoginForm, SignUpForm } from "react-customer-support";
 
 function App() {
   const okay: string = "hey";
 
   const [user, setUser] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [sendEmailVerification, setSendEmailVerification] =
+    useState<string>("");
+
   const loginHandler = () => {
     console.log("hey");
     console.log(`password ${password}`);
     console.log(`email ${user}`);
   };
 
-  return (
-    <div className="App">
-      <Button name="ellie" />
-      <LoginForm
-        joinRoom={async (okay: string) => {
-          console.log("hey");
-        }}
-        user={user}
-        setUser={setUser}
-        password={password}
-        setPassword={setPassword}
-        loginHandler={loginHandler}
-        title="Custom Title Of Ellie"
-      />
-    </div>
-  );
+  //copy paste this to see results
+
+  // <LoginForm
+  //       joinRoom={async (okay: string) => {
+  //         console.log("hey");
+  //       }}
+  //       user={user}
+  //       setUser={setUser}
+  //       password={password}
+  //       setPassword={setPassword}
+  //       loginHandler={loginHandler}
+  //       title="Custom Title Of Ellie"
+  //     />
+
+  const signUpHandler = () => {
+    console.log("hey");
+    console.log(`password ${password}`);
+    console.log(`email ${user}`);
+    console.log(`email ${setSendEmailVerification}`);
+  };
+
+  //test this 100% later
+
+  //   <SignUpForm
+  //   joinRoom={async (okay: string) => {
+  //     console.log("hey");
+  //   }}
+  //   user={user}
+  //   setUser={setUser}
+  //   password={password}
+  //   setPassword={setPassword}
+  //   signUpHandler={signUpHandler}
+  //   title="Custom Title Of Ellie"
+  //   sendEmailVerification={sendEmailVerification}
+  //   setSendEmailVerification={setSendEmailVerification}
+  // />
+
+  return <div className="App"></div>;
 }
 
 export default App;
